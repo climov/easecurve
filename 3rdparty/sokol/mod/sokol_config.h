e86fd66768dc4b0cd857d3d3419cfe19bdf5ac8e
Catalin Climov <climov@gmail.com>
2025-05-25 20:07:45 +0300

    Fixed Linux/Clang build.

--- 3rdparty/sokol/mod/sokol_config.h
+++ 3rdparty/sokol/mod/sokol_config.h
@@ -4,6 +4,9 @@
 #define SOKOL_METAL
 #elif defined(WIN32)
 #define SOKOL_D3D11
+#elif defined(__linux__)
+#define SOKOL_GLES3
+//#define SOKOL_GLCORE33
 #elif defined(__EMSCRIPTEN__)
 #define SOKOL_GLES3
 //#define SOKOL_WGPU
